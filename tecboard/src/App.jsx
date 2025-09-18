5ae988c500cd6fd7f0cf112b9c5b6f40d901ff88
Dayane Neves <dayanegcneves@outlook.com>
2025-09-18 12:04:22 -0300

    feat: inicializando projeto

---
+++ tecboard/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { FormularioDeEvento } from './components/FormularioDeEvento'
+
+function App() {
+  return (
+    <>
+      <main>
+        <header>
+          <img src="/logo.png" alt="Logomarca tecboard" />
+        </header>
+        <section>
+          <img src="/banner.png" alt="Mulher com óculos de realidade virtual" />
+        </section>
+      </main>
+      <FormularioDeEvento />
+    </>
+  )
+}
+
+export default App
