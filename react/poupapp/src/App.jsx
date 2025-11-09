ba23e767dc7eb100d34663b8a24b678b832adf2f
Dayane Neves <dayanegcneves@outlook.com>
2025-11-09 17:30:37 -0300

    organizando projetos alura

---
+++ react/poupapp/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <>
+      <aside>
+        <img src={logo} alt="Logomarca Poupapp" />
+      </aside>
+      <h1>Poupapp</h1>
+    </>
+  )
+}
+
+export default App
